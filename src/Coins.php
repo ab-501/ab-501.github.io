6226dacbf3325ed1802ef8dc40eaea399fcece21
abbyb <abdullah.butt.2021@uni.strath.ac.uk>
2024-06-20 14:33:13 +0100

    move all relevant files to root

--- src/Coins.php
+++ src/Coins.php
@@ -12,8 +12,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="../styles.css">
+    <script src="../script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Coins</title>
 </head>
